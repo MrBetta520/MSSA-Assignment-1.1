1f2ea9b774facdfd33f789245bafbff6264bac12
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-22 16:30:29 -0500

    Add files via upload
    Write a class: “Student” with private data members: StudentId ( int), StudentFname (stirng), StudentLname (string ), StudentGrade ( char ) and public properties for each data member. Instantiate the class and assign data to properties.call get and set properties.

---
+++ Assignment1.4.2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Student Management System.");
+            
+            try
+            {
+                char choice = 'y';
+                while (choice == 'y' || choice == 'Y')
+                {
+                    CallMenu();
+                    Console.WriteLine("If you want to input more student information, press y or Y. Otherwise, press any to quit.");
+
+                    choice = Convert.ToChar(Console.ReadLine());
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using this system, Goodbey!");
+                Console.ReadLine();
+            }
+        }
+
+        static void InputStudentInfo()
+        {
+            Student student = new Student();
+            Console.WriteLine("Please enter student's ID.");
+            student.StudentId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter student's last name.");
+            student.StudentLname = Console.ReadLine();
+            Console.WriteLine("Please enter student's first name.");
+            student.StudentFname = Console.ReadLine();
+            Console.WriteLine("Please enter student's grade. The grade must be A, B, C, D.");
+            student.StudentGrade = Convert.ToChar(Console.ReadLine());
+
+            while (student.StudentGrade != 'A' && student.StudentGrade != 'a' &&
+                   student.StudentGrade != 'B' && student.StudentGrade != 'b' &&
+                   student.StudentGrade != 'C' && student.StudentGrade != 'c' &&
+                   student.StudentGrade != 'D' && student.StudentGrade != 'd')
+            {
+                Console.WriteLine("Please enter A, B, C, or D");
+                student.StudentGrade = Convert.ToChar(Console.ReadLine());
+            }
+
+            student.displayStudent();
+        }
+        static void CallMenu()
+        {
+            Console.WriteLine("If you want to input student's information, press 1, any other letter to quit.");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    InputStudentInfo();
+                    Console.ReadLine();
+                    break;
+
+                default:
+                    Console.WriteLine("Please press 1.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+    
+}
---
+++ Assignment1.4.2/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._4._2
+{
+    class Student
+    {
+        private int studentId;
+        private string studentFname;
+        private string studentLname;
+        private char studentGrade;
+
+        public int StudentId
+        {
+            get;set;
+        }
+        public string StudentFname
+        {
+            get;set;
+        }
+        public string StudentLname
+        {
+            get;set;
+        }
+        public char StudentGrade
+        {
+            get;set;
+        }
+
+        public void displayStudent()
+        {
+            Console.WriteLine($"Student's ID is {this.StudentId}, name is {this.StudentFname} {this.StudentLname}, grade is {this.StudentGrade}.");
+        }
+    }
+}
