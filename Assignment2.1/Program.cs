7150bdf09a5017ad001f7943fed162d7eb85f479
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-22 16:33:39 -0500

    Add files via upload
    Write a base class: ‘Shape’ and add properties like id, name and color and method like ‘calculate area’.     
    Inherit circle shape from base class and add properties like radius. override calculate area logic for circle.        
    Inherit square class from shape and add change the calculate area logic. Add property like side of square.        
    Take the input from user to select circle or square and display the calculated area. no hard coded values!

---
+++ Assignment2.1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Yang's calculator.");
+            Console.WriteLine("Please enter y or Y to continue. Enter any other to quit.");
+            char choice = Convert.ToChar(Console.ReadLine());
+
+            while (choice == 'y' || choice == 'Y')
+            {
+                decisionMake();
+                Console.WriteLine("Please enter y or Y to continue. Enter any other to quit.");
+                choice = Convert.ToChar(Console.ReadLine());
+            }
+            Console.WriteLine("Thank you for your support. Goodbye!");
+            Console.ReadLine();
+        }
+
+        static void decisionMake()
+        {
+            Console.WriteLine("Please choose which calculation you want to make.");
+            Console.WriteLine("1. Circle");
+            Console.WriteLine("2. Square");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    areaOfCircle();
+                    Console.ReadLine();
+                    break;
+
+                case 2:
+                    areaOfSquare();
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("Please choose 1, or 2.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+
+        static void areaOfCircle()
+        {
+            Circle c = new Circle();
+            Console.WriteLine("Please enter the redius of the circle.");
+            try
+            {
+                c.redius = Convert.ToDouble(Console.ReadLine());
+                c.AreaCalculate();
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Please enter a number.");
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        static void areaOfSquare()
+        {
+            Square s = new Square();
+            Console.WriteLine("Please enter the side of the square.");
+            try
+            {
+                s.lengthOfSide = Convert.ToDouble(Console.ReadLine());
+                s.AreaCalculate();
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Please enter a number.");
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ Assignment2.1/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2._1
+{
+    abstract class Shape
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public virtual void AreaCalculate()
+        {
+            Console.WriteLine("Let's do some math.");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public double redius { get; set; }
+
+        public override void AreaCalculate()
+        {
+            double area = Math.PI * Math.Pow(redius, 2);
+            string areaString = String.Format("The area of this circle is {0:0.00}.", area);
+            Console.WriteLine(areaString);
+        }
+    }
+
+    class Square : Shape
+    {
+        public double lengthOfSide { get; set; }
+        public override void AreaCalculate()
+        {
+            double area = Math.Pow(lengthOfSide, 2);
+            string areaString = String.Format("The area of this square is {0:0.00}.", area);
+            Console.WriteLine(areaString);
+        }
+    }
+}
