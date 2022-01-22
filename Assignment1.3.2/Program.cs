4e8b21edc0328e705eea7c2bdaad9a429d87b8ab
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-22 16:11:46 -0500

    Add files via upload
    Write a program in C# to read n number of values in an array and display it in reverse order.

---
+++ Assignment1.3.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer for the array size.");
+            int size = Convert.ToInt32(Console.ReadLine());
+            arrayTest(size);
+
+            Console.ReadLine();
+        }
+
+        static void arrayTest(int length)
+        {
+            int[] a1 = new int[length];
+            
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("element - 0: ");
+                a1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The values store into the array are: ");
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(a1[i] + " ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("The values store into the array in reverse are: ");
+
+            for (int i = length -1; i >= 0; i--)
+            {
+                Console.Write(a1[i] + " ");
+            }
+            
+        }
+    }
+}
