ac5f393d971954fd6a883322f9db51e382d980c0
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-22 10:07:47 -0500

    Add files via upload
    Write a program in C# to calculate area of triangle, square and rectangle. Write 3 different functions for each shape to take dimensions of figure and display the area. You may create menus.

---
+++ Assignment1.3.1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char choice = 'y';
+
+            while(choice == 'y' || choice == 'Y')
+            {
+                AreaCalculation();
+                Console.WriteLine("If you want to continue calculation, press y or Y. Press any other button to quit.");
+
+                choice = Convert.ToChar(Console.ReadLine());
+            }
+        }
+
+        static void AreaCalculation()
+        {
+            Console.WriteLine("Please select one operation you want to do.");
+            Console.WriteLine("1. Triangle");
+            Console.WriteLine("2. Square");
+            Console.WriteLine("3. Rectangle");
+            Console.WriteLine("Enter your choice");
+
+            int choice = Int32.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Triangle();
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Square();
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    Rectangle();
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("Please enter 1, 2 or 3.");
+                    Console.ReadLine();
+                    break;                
+            }
+           
+            
+        }
+        static void Triangle()
+        {
+            double num1, num2, result;
+
+            Console.WriteLine("Please enter the length of triangle.");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the height of triangle.");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            result = 0.5 * num1 * num2;
+            Console.WriteLine($"The area of this triangle is: {result}.");
+        }
+
+        static void Square()
+        {
+            double num, result;
+
+            Console.WriteLine("Please enter the length of the square.");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            result = Math.Pow(num, 2);
+            Console.WriteLine($"The area of this square is: {result}.");
+        }
+
+        static void Rectangle()
+        {
+            double length, width, result;
+
+            Console.WriteLine("Please enter the length of the rectangle.");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the width of the rectangle.");
+            width = Convert.ToDouble(Console.ReadLine());
+
+            result = length * width;
+            Console.WriteLine($"The area of this rectangle is: {result}.");
+        }
+    }
+}
