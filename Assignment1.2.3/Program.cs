f42e6e988671cb8bf04dde1b7dd358096a0129a4
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-22 09:54:10 -0500

    Add files via upload
    Write a C# Sharp program to accept two integers and check whether they are equal or not.

---
+++ Assignment1.2.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] nums = new int[10];
+
+            for (int i = 1; i <= 10; i++)
+            {
+                sum += i;
+                nums[i - 1] = i;
+            }
+
+            Console.WriteLine("The first 10 natural number is: ");
+            foreach(int num in nums)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("  ");
+            Console.WriteLine($"The Sum is: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
