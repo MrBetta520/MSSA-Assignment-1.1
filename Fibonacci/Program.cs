efab38dcfe28a996dc5b1f8e47c55db3e0b0acf8
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-22 16:37:10 -0500

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fibonacci(9);
+            Console.ReadLine();
+        }
+        static void fibonacci (int length)
+        {
+            int temp = 0;
+            int a = 0;
+            int b = 1;
+            Console.Write(a + " ");
+            for (int i = 0; i < length - 1; i++)
+            {
+                temp = a;
+                a = b;
+                b = temp + a;
+                Console.Write(a + " ");
+            }
+
+        }
+    }
+}
