7c2900561dadcd19eb2f550bd4f42c075e3c3bcf
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-22 16:28:52 -0500

    Add files via upload

---
+++ Assignment1.4.1/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._4._1
+{
+    struct Point
+    {
+        private double _x;
+        private double _y;
+
+        //public Point(double x, double y)
+        //{
+        //    this._x = x;
+        //    this._y = y;
+        //}
+
+        public double X
+        {
+            get
+            {
+                return this._x;
+            }
+            set
+            {
+                this._x = value;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return this._y;
+            }
+            set
+            {
+                this._y = value;
+            }
+        }
+    }
+}
