f18c2b5853a8c0d52ddc2bdbbc87b580182a4ea4
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-22 09:59:08 -0500

    Add files via upload
    Write a C# Sharp program to accept two integers and check whether they are equal or not.

---
+++ Assignment1.2.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another number.");
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(CompareNums(num1, num2));
+
+            Console.ReadLine();
+
+        }
+
+        static string CompareNums (int num1, int num2)
+        {
+            string result;
+
+            if (num1 == num2)
+            {
+                result = num1 + " and " + num2 + " are equal.";
+            }
+            else if(num1 > num2)
+            {
+                result = num1 + " is greater than " + num2;
+            }
+            else
+            {
+                result = num1 + " is less than " + num2;
+            }
+
+            return result;
+        }
+    }
+}
