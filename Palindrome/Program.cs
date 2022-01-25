fd94d2aec1dfb1b1fdbd67bfa81057c49a756d23
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-24 22:17:30 -0500

    Add files via upload
    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
    
    Given a string s, return true if it is a palindrome, or false otherwise.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string.");
+            string s = Console.ReadLine();
+            Palindrome(s);
+
+            Console.ReadLine();
+        }
+        static void Palindrome (string s)
+        {
+
+            s = Regex.Replace(s, "[^a-zA-Z0-9]", String.Empty);
+
+            char[] c = s.ToCharArray();
+
+            Array.Reverse(c);
+            string y = new string(c);
+
+            if (s.ToLower().Equals(y.ToLower()))
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+        }
+    }
+}
