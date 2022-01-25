0eec8119abccc3efd43e4631cbda584d06366600
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-24 22:45:54 -0500

    Add files via upload
    Write a function that reverses a string. The input string is given as an array of characters s.
    
    You must do this by modifying the input array in-place with O(1) extra memory.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void ReverseString(char[] s)
+        {
+            int left = 0, right = s.Length - 1;
+
+            while (left < right)
+            {
+                char tmp = s[left];
+                s[left++] = s[right];
+                s[right--] = tmp;
+            }
+        }
+    }
+}
