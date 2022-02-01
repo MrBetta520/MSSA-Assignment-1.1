a38e1454153f7b922f5f2b221cd2679ea8da3ca1
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-01-31 19:56:50 -0500

    Add files via upload

---
+++ Assignment 4.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReturnEvenNumvers());
+            Console.ReadLine();
+        }
+
+        static string ReturnEvenNumvers ()
+        {
+            StringBuilder s = new StringBuilder();
+            for(int i = 2; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    s.Append(i.ToString() + " ");
+                }
+            }
+
+            return s.ToString();
+        }
+    }
+}
